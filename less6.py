fc34fe7863868500479099f31a707539e783d0e1
JEMaurel <estevanmaurel@gmail.com>
2024-10-02 23:45:40 -0300

    enciende led con pull-up button

--- less6.py
+++ less6.py
@@ -1,16 +1,24 @@
-#pull up / down / segun la conexion . el script es el mismo 
+#pull up  /mas led/ el script es el mismo 
 #https://www.youtube.com/watch?v=0OYtR8UdZQk&list=PLGs0VKk2DiYxdMjCJmcP6jt4Yw6OHK85O&index=13
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
+delay=.1
 inPin=40
+outPin=38
 GPIO.setup(inPin,GPIO.IN)
+GPIO.setup(outPin,GPIO.OUT)
 from time import sleep
 try:
 	while True:
 		readVal=GPIO.input(inPin)
-		print(readVal)
-		sleep(1)
+		if readVal == 1:
+			GPIO.output(outPin,0)
+		if readVal ==0:
+			GPIO.output(outPin,1)
 except KeyboardInterrupt:
 	GPIO.cleanup()
+	print('thats all folks')
+
+
 
